1961b657916589bf1fbc26d4f045f6dfee01410d
yu2C <c2001413@gmail.com>
2026-06-18 18:40:08 +0800

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.rs
@@ -0,0 +1,45 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//     pub val: i32,
+//     pub left: Option<Rc<RefCell<TreeNode>>>,
+//     pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         TreeNode {
+//             val,
+//             left: None,
+//             right: None,
+//         }
+//     }
+// }
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut ans = 0;
+
+        fn h(root: Option<Rc<RefCell<TreeNode>>>, ans: &mut i32) -> i32 {
+            match root {
+                None => 0,
+
+                Some(node) => {
+                    let node = node.borrow();
+                    let l = h(node.left.clone(), ans);
+                    let r = h(node.right.clone(), ans);
+
+                    *ans = (*ans).max(l + r);
+                    
+                    l.max(r) + 1
+                }
+            }
+        }
+        h(root, &mut ans);
+        ans
+    }
+}
