23295acd3e7d5aabbef846c525cc8ee5dde01027
yu2C <c2001413@gmail.com>
2026-07-07 00:42:56 +0800

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn max_area(heights: Vec<i32>) -> i32 {
+		let mut l = 0;
+		let mut r = heights.len()-1;
+
+		let mut v:i32 = 0;
+
+		while l < r {
+			let h = heights[l].min(heights[r]);
+			let w = r - l;
+			let cur_v = h as i32 * w as i32;
+
+			v = v.max(cur_v);
+
+			if heights[l] < heights[r] {
+				l += 1;
+			} else {
+				r -= 1;
+			}
+		}
+		v
+    }
+}
