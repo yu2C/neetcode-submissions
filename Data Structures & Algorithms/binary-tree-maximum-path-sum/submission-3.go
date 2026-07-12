fa09656d877aeb9848f2a14630a942ae34298f4e
yu2C <c2001413@gmail.com>
2026-07-12 11:11:40 +0800

    Add: binary-tree-maximum-path-sum - submission-3

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-3.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxPathSum(root *TreeNode) int {
+    ans := math.MinInt32
+
+    var dfs func(node *TreeNode) int
+
+    dfs = func(node *TreeNode) int {
+        if node == nil {
+            return 0
+        }
+
+        left := max(0, dfs(node.Left))
+        right := max(0, dfs(node.Right))
+        ans = max(ans, node.Val + left + right)
+
+        return node.Val + max(left, right)
+    }
+    dfs(root)
+    return ans
+}
