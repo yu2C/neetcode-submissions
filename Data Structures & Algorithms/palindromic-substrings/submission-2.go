9f05701759b77e928f1e33d219966cd107e2d3da
yu2C <c2001413@gmail.com>
2026-08-21 01:33:53 +0800

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.go
@@ -0,0 +1,24 @@
+func countSubstrings(s string) int {
+    if len(s) == 0 {
+        return 0
+    }
+    var expand func(left int, right int) int
+    expand = func(left int, right int) int {
+        count := 0
+
+        for left >= 0 && right < len(s) && s[left] == s[right] {
+            count++
+            left--
+            right++
+        }
+        return count
+    }
+    res := 0
+    for i := range s {
+        // Odd palindrome
+        res += expand(i, i)
+        // Even palindrome
+        res += expand(i, i+1)
+    }
+    return res
+}
