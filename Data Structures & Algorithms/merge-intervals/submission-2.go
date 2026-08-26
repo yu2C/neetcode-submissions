f57938912af8c9a05162d9f5de86fb586f43bed8
yu2C <c2001413@gmail.com>
2026-08-26 17:04:11 +0800

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.go
@@ -0,0 +1,18 @@
+func merge(intervals [][]int) [][]int {
+    res := make([][]int, 0, len(intervals))
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i][0] < intervals[j][0]
+    })
+    res = append(res, intervals[0])
+
+    for i := 1; i < len(intervals); i++ {
+        if res[len(res)-1][1] >= intervals[i][0] {
+            res[len(res)-1][0] = min(res[len(res)-1][0], intervals[i][0])
+            res[len(res)-1][1] = max(res[len(res)-1][1], intervals[i][1])
+            
+        } else {
+            res = append(res, intervals[i])
+        }
+    }
+    return res
+}
