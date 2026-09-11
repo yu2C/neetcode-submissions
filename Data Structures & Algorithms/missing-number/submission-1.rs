8fd92612ef2d1997245ac6f2f9035fd34128d91a
yu2C <c2001413@gmail.com>
2026-09-11 21:08:01 +0800

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let mut xor = nums.len() as i32;
+
+        for (i, num) in nums.iter().enumerate() {
+            xor ^= i as i32;
+            xor ^= *num;
+        }
+
+        xor
+    }
+}
